06fd51c5200401a2d51074d3da99251b83de5c3c
Hai Nguyen <s3978275@rmit.edu.vn>
2023-08-11 05:16:30 +0700

    tweak contact page

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function limitPhoneNumberLength(input) {
         input.value = input.value.slice(0, maxLength);
     }
 }
+
+// Get the input fields and dropdown
+const countryDropdown = document.getElementById("country-dropdown");
+const phoneInput = document.getElementById("phone");
+
+// Add event listener to country dropdown
+countryDropdown.addEventListener("change", updatePhoneNumber);
+
+// Function to update the phone number field
+function updatePhoneNumber() {
+    // Get the selected country code from the dropdown
+    const countryCode = countryDropdown.value;
+
+    // Set the country code in the phone number input
+    phoneInput.value = countryCode + phoneInput.value;
+}
